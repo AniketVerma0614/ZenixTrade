c976c90c9e45d6642e9673ec0fd3d1efb461cb6f
AniketVerma0614 <aniketverma0614@gmail.com>
2025-03-03 14:01:27 +0530

    Navbar,Footer & Open Account

--- frontend/src/landing_page/home/Pricing.js
+++ frontend/src/landing_page/home/Pricing.js
@@ -1,33 +1,42 @@
-import React from 'react';
+import React from "react";
 
 function Pricing() {
-    return ( 
-        <div className='container mb-5'>
-            <div className='row'>
-                <div className='col-4'>
-                    <h1 className='mb-3 fs-2'>Unbeatable pricing</h1>
-                    <p>We pioneered the concept of discount broking and price transparency in India. Falt and no hidden charges.</p>
+  return (
+    <div className="container mb-5">
+      <div className="row">
+        <div className="col-4">
+          <h1 className="mb-3 fs-2">Unbeatable pricing</h1>
+          <p>
+            We pioneered the concept of discount broking and price transparency
+            in India. Falt and no hidden charges.
+          </p>
 
-                    <a href='' style={{textDecoration:"none"}}>Try Kite Demo  <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
-                </div>
-
-                <div className='col-2'></div>
-
-            <div className='col-6'>
-                <div className='row text-center'>
-                    <div className='col p-3 border'>
-                        <h1 className='mb-3'>Rs 0</h1>
-                        <p>Free equity delivery and <br/>direct mutual funds</p>
-                    </div>
-                    <div className='col p-3 border'>
-                        <h1 className='mb-3'>RS 20</h1>
-                        <p>Intraday and F&O</p>
-                    </div>
-                </div>
-            </div>
-            </div>
+          <a href="" style={{ textDecoration: "none" }}>
+            Try Kite Demo{" "}
+            <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+          </a>
         </div>
-     );
+
+        <div className="col-2"></div>
+
+        <div className="col-6">
+          <div className="row text-center">
+            <div className="col p-3 border">
+              <h1 className="mb-3">Rs 0</h1>
+              <p>
+                Free equity delivery and <br />
+                direct mutual funds
+              </p>
+            </div>
+            <div className="col p-3 border">
+              <h1 className="mb-3">RS 20</h1>
+              <p>Intraday and F&O</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Pricing;
