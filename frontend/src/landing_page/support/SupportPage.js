a94459f6083d80995980d7d4b36483eba7784777
AniketVerma0614 <aniketverma0614@gmail.com>
2025-03-05 19:48:47 +0530

    "Support" Component (Part 3)

--- frontend/src/landing_page/support/SupportPage.js
+++ frontend/src/landing_page/support/SupportPage.js
@@ -1,19 +1,18 @@
 import React from "react";
 
-import Hero from './Hero';
-import CreateTicket from './CreateTicket';
+import Hero from "./Hero";
+import CreateTicket from "./CreateTicket";
 
+import Navbar from "../Navbar";
+import Footer from "../Footer";
 
-import Navbar from '../Navbar';
-import Footer from '../Footer';
-
-function PricingPage(){
-    return (
-        <>
-            <Hero/>
-            <CreateTicket/>
-        </>
-    );
+function PricingPage() {
+  return (
+    <>
+      <Hero />
+      <CreateTicket />
+    </>
+  );
 }
 
 export default PricingPage;
