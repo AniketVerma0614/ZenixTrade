b6e136318b62b72f0dfd309e6475d7738b356af5
AniketVerma0614 <aniketverma0614@gmail.com>
2025-03-05 13:49:32 +0530

    RightSection in "Product" Component

--- frontend/src/landing_page/products/ProductsPage.js
+++ frontend/src/landing_page/products/ProductsPage.js
@@ -26,7 +26,11 @@ function ProductPage() {
         appStore=""
       />
 
-      <RightSection />
+      <RightSection
+        imageURL="media\images\console.png"
+        productName="Console"
+        productDescription="The central dashboard for your Zerodha account. Gain insights into your trades and investments with in-depth reports and visualisations."
+      />
 
       <LeftSection
         imageURL="media\images\coin.png"
@@ -38,7 +42,11 @@ function ProductPage() {
         appStore=""
       />
 
-      <RightSection />
+      <RightSection
+        imageURL="media\images\kiteconnect.png"
+        productName="Kite Connect API"
+        productDescription="Build powerful trading platforms and experiences with our super simple HTTP/JSON APIs. If you are a startup, build your investment app and showcase it to our clientbase."
+      />
 
       <LeftSection
         imageURL="media\images\varsity.png"
@@ -47,11 +55,8 @@ function ProductPage() {
         tryDemo=""
         learnMore=""
         googlePlay=""
-        appStore=""
       />
-      
-      <RightSection />
-
+      <p className="text-center mt-5 mb-5">Want to know more about our technology stack? Check out the Zerodha.tech.blog</p>
       <Universe />
     </>
   );
